a519f69a0b22d738980361e8997bcee634a997d3
John Dalton <jldalton@containerstore.com>
2013-12-09 17:47:29 -0600

    added unit tests for product price

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,44 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "product attributes must not be empty" do
+      product = Product.new
+      assert product.invalid?
+      assert product.errors[:title].any?
+      assert product.errors[:description].any?
+      assert product.errors[:price].any?
+      assert product.errors[:image_url].any?
+  end
+
+
+  test "price must not be negative" do
+      product = Product.new(title: "My Book Title",
+          description: "yyy",
+          image_url: "zzz.jpg")
+      product.price = -1
+      assert product.invalid?
+      assert_equal ["must be greater than or equal to 0.01"],
+          product.errors[:price]
+  end
+
+  test "price must not be zero" do
+      product = Product.new(title: "My Book Title",
+          description: "yyy",
+          image_url: "zzz.jpg")
+
+      product.price = 0
+      assert product.invalid?
+      assert_equal ["must be greater than or equal to 0.01"],
+          product.errors[:price]
+  end
+
+  test "price must be positive" do
+      product = Product.new(title: "My Book Title",
+          description: "yyy",
+          image_url: "zzz.jpg")
+      product.price = 0.01
+      assert product.valid?
+  end
+
 end
