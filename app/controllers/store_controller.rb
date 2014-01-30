14d53c0eaf33b2e9731fdfe41c75696562c49dbe
John Dalton <jldalton@containerstore.com>
2014-01-29 23:15:44 -0600

    up thru 11.4

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,8 @@
 class StoreController < ApplicationController
-  def index
-      @products = Product.order(:title)
-      @count = session_count(:store)
+  include CurrentCart
+  before_action :set_cart   
+  def index     
+    @products = Product.order(:title)
+    @count = session_count(:store)
   end
 end
