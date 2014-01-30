76403ce525611ba5dc04c879cd45032b0f01d95b
John Dalton <jldalton@containerstore.com>
2014-01-30 01:21:34 -0600

    ch 11 ex 2 no ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -60,9 +60,14 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    if @line_item.quantity > 1
+      @line_item.quantity -= 1
+      @line_item.save
+    else
+      @line_item.destroy
+    end
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
